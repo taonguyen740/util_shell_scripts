4d3d4dddb7ae70dcdfa04924f15d56b148e31d4e
taonguyen740 <dinhtao995@gmail.com>
2022-07-26 16:25:07 +0900

    EC2(Linux)の操作のログscriptlog追加

---
+++ scriptlog.sh
@@ -0,0 +1,31 @@
+#===================================
+# Script Name   : scriptlog.sh
+# Description   : This script outputs the operation log for each user to the specified directory.
+#===================================
+
+LOGDIR=/var/log/script
+LOGFILE=\`whoami\`.log
+
+P_PROC=\`ps -ef|grep \$PPID|grep bash|awk '{print \$8}'\`
+
+if [ "\`whoami\`" != "root" ] && [ "\$P_PROC" = -bash ];then
+  #/bin/script -faq \${LOGFILE}
+  # タイムスタンプを追加
+  /bin/script -faq >(awk '{print strftime("%Y-%m-%d %H:%M:%S "), \$0} {fflush() }'>> \${LOGDIR}/\${LOGFILE})
+  exit
+fi
+EOF
+
+cat <<EOF >/etc/logrotate.d/scriptlog
+/var/log/script/*.log
+{
+    daily
+    rotate 93
+    missingok
+    notifempty
+    copytruncate
+    dateext
+    dateformat .%Y%m%d
+    compress
+    su root root
+}
