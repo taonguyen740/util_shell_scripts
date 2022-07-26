b8086155317b88e2b44cc9f29dc2cbead354a024
taonguyen740 <dinhtao995@gmail.com>
2022-07-26 18:49:11 +0900

    Update scriptlog.sh

--- scriptlog.sh
+++ scriptlog.sh
@@ -1,3 +1,6 @@
+mkdir /var/log/script/OLD_20220726
+mv /var/log/script/root_* /var/log/script/OLD_20220726
+mv /var/log/script/centos_* /var/log/script/OLD_20220726
 chmod 644 /etc/profile.d/scriptlog.sh
 cat <<EOF >/etc/profile.d/scriptlog.sh
 #===================================
